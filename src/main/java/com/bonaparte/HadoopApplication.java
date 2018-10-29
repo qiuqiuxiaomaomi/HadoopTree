eb483475255b4d074b8045e07a6cbba1d813b09b
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-10-29 18:34:54 +0800

    Hadoop工程初始化

---
+++ src/main/java/com/bonaparte/HadoopApplication.java
@@ -0,0 +1,7 @@
+package com.bonaparte;
+
+/**
+ * Created by yangmingquan on 2018/10/29.
+ */
+public class HadoopApplication {
+}
--- src/main/java/com/bonaparte/job/MaxMoneyJob.java
+++ src/main/java/com/bonaparte/job/MaxMoneyJob.java
@@ -1,10 +1,10 @@
-package com.bonaparte;
+package com.bonaparte.job;
 
 /**
  * Created by yangmingquan on 2018/10/28.
  */
 public class MaxMoneyJob {
     public static void main(String[] args){
-        
+
     }
 }
